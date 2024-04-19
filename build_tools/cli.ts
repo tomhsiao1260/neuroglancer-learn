09b9b44637a556e0a1287f81d360bb44e08166cb
Hsiao Yao <tomtogo1260@gmail.com>
2024-04-20 00:46:19 +0800

    yargs init

--- build_tools/cli.ts
+++ build_tools/cli.ts
@@ -1,8 +1,34 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import process from "node:process";
+import yargs from "yargs";
 
-console.log(process.argv[1])
-console.log(process.argv[2])
+function parseArgs() {
+  const instance = yargs(process.argv.slice(2))
+  console.log(process.argv.slice(2))
+  console.log(instance)
 
-console.log(import.meta.url)
-console.log(import.meta.dirname)
-console.log(import.meta.filename)
+  return instance
+}
+
+async function parseArgsAndRunMain() {
+  parseArgs();
+}
+
+if (process.argv[1] === import.meta.filename) {
+  parseArgsAndRunMain();
+}
